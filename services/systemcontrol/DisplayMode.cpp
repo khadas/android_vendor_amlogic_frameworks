363b9073372ef55206525f07940fcef85cce1111
Jinping Wang <jinping.wang@amlogic.com>
2016-09-22 11:28:23 +0800

    PD #132262: dont't copy displaymode to outputmode
    Change-Id: I03623fd72909c1b8eac5daf4f41ac8879ca48b30

--- services/systemcontrol/DisplayMode.cpp
+++ services/systemcontrol/DisplayMode.cpp
@@ -578,11 +578,6 @@ void DisplayMode::setMboxDisplay(char* hpdstate, output_mode_state state) {
             pSysWrite->writeSysfs(DISPLAY_FB1_BLANK, "1");
             pSysWrite->writeSysfs(DISPLAY_FB1_FREESCALE, "0");
         }
-        //when tvserver change display mode, need keep pace with outputmode
-        if (strlen(data.current_mode) != 0 && !strcmp(data.hpd_state, "1") &&
-                strstr(data.current_mode, "cvbs") == NULL) {
-            strcpy(outputmode, data.current_mode);
-        }
     }
     setMboxOutputMode(outputmode, state);
 }

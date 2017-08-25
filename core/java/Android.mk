d134f1467ea41c69742359c2a46cbcef19fd4f65
Chaomin Zheng <chaomin.zheng@amlogic.com>
2017-08-25 14:50:52 +0800

    droidlogic:generate amlogic files to vendor[2/2]
    PD# 149599
    
    generate amlogic files to vendor
    
    Change-Id: I4315c11b853f6acedce853c442cb6b7c5f5c7a38

--- core/java/Android.mk
+++ core/java/Android.mk
@@ -11,4 +11,8 @@ LOCAL_MODULE := droidlogic
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_DX_FLAGS := --core-library
 
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+LOCAL_PROPRIETARY_MODULE := true
+endif
+
 include $(BUILD_JAVA_LIBRARY)

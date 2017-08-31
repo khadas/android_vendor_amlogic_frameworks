6b4a7d0e93f04d71a9897357aff3f034946d9b5c
Tellen Yu <tellen.yu@amlogic.com>
2017-08-31 18:44:30 +0800

    droidlogic: use-library instead of bootcalss [1/11]
    PD# NONE
    
    droidlogic.jar removed from BOOTCLASSPATH,
    because BOOTCLASSPATH's prefix also /system/framework/*.jar,
    in treble mode, we can not put any vendor file to /system
    
    Change-Id: I0ca8d2c6b3783abae133650d2fd75c35f2814cfa

--- core/java/Android.mk
+++ core/java/Android.mk
@@ -16,3 +16,19 @@ LOCAL_PROPRIETARY_MODULE := true
 endif
 
 include $(BUILD_JAVA_LIBRARY)
+
+#copy xml to permissions directory
+include $(CLEAR_VARS)
+LOCAL_MODULE := droidlogic.software.core.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/permissions
+else
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+endif
+
+include $(BUILD_PREBUILT)

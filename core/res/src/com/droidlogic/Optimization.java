45c95ad4c02f0842ca1cd4f314412dc6f271b60b
xiaoliang.wang <xiaoliang.wang@amlogic.com>
2017-11-21 15:12:03 +0800

    Optimization: change for endoder gts test[1/2]
    PD#154662
    
    change for endoder gts test
    
    Change-Id: I26e22dd7e92bc560b947e78a07d232c22e0be98e

--- core/res/src/com/droidlogic/Optimization.java
+++ core/res/src/com/droidlogic/Optimization.java
@@ -53,20 +53,36 @@ public class Optimization extends Service {
 
     private Runnable runnable = new Runnable() {
         public void run() {
+            int retProc = -1;
+            int retPkg = -1;
             ActivityManager am = (ActivityManager)mContext.getSystemService(Context.ACTIVITY_SERVICE);
 
             while (true) {
                 try {
-                    List< ActivityManager.RunningTaskInfo > task = am.getRunningTasks (1);
-                    if (!task.isEmpty()) {
-                        ComponentName cn = task.get (0).topActivity;
-                        String pkg = cn.getPackageName();
-                        String cls = cn.getClassName();
+                    if (retProc != 0 && retProc != -4) {
+                        List< ActivityManager.RunningTaskInfo > task = am.getRunningTasks (1);
+                        if (!task.isEmpty()) {
+                            ComponentName cn = task.get (0).topActivity;
+                            String pkg = cn.getPackageName();
+                            String cls = cn.getClassName();
 
-                        nativeOptimization(pkg, cls);//bench match
+                            retPkg = nativeOptimization(pkg, cls);//bench match
+                        }
                     }
 
-                    Thread.sleep(500);
+                    if (retPkg != 0 && retPkg != -4) {
+                        List< ActivityManager.RunningAppProcessInfo> apInfo = am.getRunningAppProcesses();
+                        int len = apInfo.size();
+                        //Log.i(TAG, "apInfo.size():" + len);
+                        String [] proc = new String[len];
+                        for (int i = 0; i < len; i++) {
+                            //Log.i(TAG, "apInfo[" + i + "] processName:" + apInfo.get(i).processName);
+                            proc[i] = apInfo.get(i).processName;
+                        }
+                        retProc = nativeOptimization(proc);
+                    }
+
+                    Thread.sleep(50);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -75,4 +91,5 @@ public class Optimization extends Service {
     };
 
     private native int nativeOptimization(String pkg, String cls);
+    private native int nativeOptimization(String[] proc);
 }

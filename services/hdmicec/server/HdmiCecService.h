4da906b2179281038c79080750ff035cbb6fde78
an.xi <an.xi@amlogic.com>
2018-11-16 17:32:43 +0800

    NativeImagePlayer: Add amlogic license for amlogic apps [1/1]
    PD#SWPL-2111
    
    Problem:
    no amlogic license for amlogic apps
    
    Solution:
    Add amlogic license for amlogic apps
    
    Verify:
    verify it on Ampere
    
    Change-Id: Id36af58ab741fc2511faf3466ed92e29a000b8e6
    Signed-off-by: an.xi <an.xi@amlogic.com>

--- services/hdmicec/server/HdmiCecService.h
+++ services/hdmicec/server/HdmiCecService.h
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2014 Amlogic, Inc. All rights reserved.
+ *
+ * This source code is subject to the terms and conditions defined in the
+ * file 'LICENSE' which is part of this source code package.
+ *
+ * Description:
+ *     AMLOGIC
+ */
+
 #ifndef _HDMI_CEC_SERVICE_H_
 #define _HDMI_CEC_SERVICE_H_
 
--- services/imageplayer/ImagePlayerService.cpp
+++ services/imageplayer/ImagePlayerService.cpp
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2014 Amlogic, Inc. All rights reserved.
+ *
+ * This source code is subject to the terms and conditions defined in the
+ * file 'LICENSE' which is part of this source code package.
+ *
+ * Description:
+ *     AMLOGIC
+ */
+
 /** @file ImagePlayerService.cpp
  *  @par Copyright:
  *  - Copyright 2011 Amlogic Inc as unpublished work

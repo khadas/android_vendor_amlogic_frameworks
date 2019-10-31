5965749393f806f9b3f81bf9c45e3e0a25b043dc
wenjie.chen <wenjie.chen@amlogic.com>
2019-10-31 11:12:08 +0800

    SELinux: MediaServer hasn‘t permission to seprop on AndroidP. [2/5]
    PD#OTT-7073
    
    Problem:
    MediaServer hasn‘t permission to seprop on AndroidP.
    
    Solution:
    MediaServer goes to setprop through systemcontrolservice.
    
    Verify:
    U212
    
    Change-Id: I4158412bb28c57f9baa8a36534389ac0c1783728
    Signed-off-by: wenjie.chen <wenjie.chen@amlogic.com>

--- services/systemcontrol/Android.mk
+++ services/systemcontrol/Android.mk
@@ -33,35 +33,6 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES:= \
-  SystemControlClient.cpp
-
-LOCAL_SHARED_LIBRARIES := \
-  libutils \
-  libcutils \
-  liblog
-
-LOCAL_SHARED_LIBRARIES += \
-  vendor.amlogic.hardware.systemcontrol@1.0 \
-  vendor.amlogic.hardware.systemcontrol@1.1 \
-  libbase \
-  libhidlbase \
-  libhidltransport
-
-LOCAL_C_INCLUDES += \
-  $(BOARD_AML_VENDOR_PATH)/frameworks/services/systemcontrol/PQ/include
-
-LOCAL_MODULE:= libsystemcontrolclient
-
-LOCAL_MODULE_TAGS := optional
-
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
-LOCAL_PROPRIETARY_MODULE := true
-endif
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
 ifeq ($(TARGET_BOARD_PLATFORM), meson8)
 LOCAL_CFLAGS += -DMESON8_ENVSIZE
 endif

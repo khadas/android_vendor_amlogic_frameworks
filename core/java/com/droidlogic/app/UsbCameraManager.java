3d24703750d634817d0a9e101b15b414b8feb5eb
jason <jason@khadas.com>
2022-06-14 15:00:25 +0000

    camera: enable camera app
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: Ib982bde5398f69db8b1eee94bd6c2a03029e51a8

--- core/java/com/droidlogic/app/UsbCameraManager.java
+++ core/java/com/droidlogic/app/UsbCameraManager.java
@@ -214,11 +214,7 @@ public class UsbCameraManager {
                         Log.i(TAG, "/dev/video* num:" + devNum);
                     }
                 }
-                if (mIsAttach &&
-                    //device path has been set up by kernel
-                    ((devNum > mCamNum) ||
-                    //video device was plugged in when boot
-                    ((devNum > 0) && (mCamNum == devNum)))) {
+                if (mIsAttach && devNum > 0) {
                     usbCameraAttach(mIsAttach);
                     for (int i = 0; i < ACTIVITIES.length; i++) {
                         enableComponent(PACKAGES[i], ACTIVITIES[i]);
